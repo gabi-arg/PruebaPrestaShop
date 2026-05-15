b410aaeef7a660f3ebc95ad760c762d39e8a8d7d
gabi-arg <gabrieladuran013@gmail.com>
2026-05-16 00:19:43 +0200

    Add es and en translations

---
+++ modules/productbadges/translations/es.php
@@ -0,0 +1,5 @@
+<?php
+global $_MODULE;
+$_MODULE = [];
+$_MODULE['<{productbadges}prestashop>productbadges_'] = 'Product Badges';
+$_MODULE['<{productbadges}prestashop>productbadges_abc123'] = 'Gestión de etiquetas visuales para productos del catálogo.';
